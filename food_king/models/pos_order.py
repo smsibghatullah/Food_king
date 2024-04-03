348db013c3a2d0e7e108bec4ef78dc0686913c0b
mubeen1020 <m.mubeen1020@gmail.com>
2024-04-03 08:14:52 +0500

    working

--- food_king/models/pos_order.py
+++ food_king/models/pos_order.py
@@ -8,6 +8,18 @@ class pos_order_food_king(models.Model):
     status = fields.Char('Order Status')
 
     def accept_order(self):
+        search_kitchen = self.env['pos_preparation_display.display'].search([('name', '=', 'Food King Kitchen')]).mapped('id')
+        search_pos = self.env['pos.config'].search([('name', '=', 'Food King Pos')]).mapped('id')
+        if search_kitchen:
+            print("already exist")
+        else : 
+            vals = {
+                'name': 'Food King Kitchen',
+                'company_id': self.env.company.id,
+                'pos_config_ids': search_pos,
+                
+            }
+            self.env['pos_preparation_display.display'].create(vals)
         food_king = self.env['food_king.food_king'].sudo().search([], limit=1)
         if not food_king:
              print('Food King settings not configured. Please configure Food King settings first.')
@@ -29,5 +41,5 @@ class pos_order_food_king(models.Model):
         if pos_data:
             self.state = 'draft'
         print(pos_data,"ggggggggggggggg")
-        # self.env['pos_preparation_display.display'].create(vals)
+
         
