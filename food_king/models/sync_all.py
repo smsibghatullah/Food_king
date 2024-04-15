7c1a22bb7fb6d2a5738aeec176c062919dbc7778
mubeen1020 <m.mubeen1020@gmail.com>
2024-04-15 06:43:56 +0500

    working

--- food_king/models/sync_all.py
+++ food_king/models/sync_all.py
@@ -571,7 +571,7 @@ class food_king(models.Model):
                                     'discount': float(discount),
                                     'tax_ids': [(6, 0, [int(product_tax)])] if product_tax else False,
                                     'price_subtotal': float(posid['total_convert_price']),
-                                    'price_subtotal_incl': float(posid['total_convert_price']) + (float(posid['total_convert_price']) * float(product_tax) if product_tax else 0) / 100
+                                    'price_subtotal_incl': float(posid['total_convert_price']) + (float(posid['total_convert_price']) * float(product_tax.amount) if product_tax else 0) / 100
                                 }))
                             if customer_ids:
                                 search_table = self.env['restaurant.table'].search([('name', '=',pos_data['table_name'] )]).mapped('id')
